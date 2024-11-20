f5c549c62925250d5f6a19639d3f870902605f21
L.B. Deyo <lbdeyo@gmail.com>
2024-11-20 17:29:44 -0600

    Updating hero image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -153,7 +153,7 @@ export default function Home() {
         <div
           className="fixed inset-0 w-full h-full bg-cover bg-center bg-no-repeat opacity-0"
           style={{
-            backgroundImage: "url('/img/rr-hero-image.jpg')",
+            backgroundImage: "url('/img/rr-hero-image-fixed.jpg')",
             animation: "fadeIn 13s ease-in forwards",
             zIndex: -20,
             backgroundAttachment: "fixed",
