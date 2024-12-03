26b5e187a311ccda63c61f26dccb4b5d6ebc9358
L.B. Deyo <lbdeyo@gmail.com>
2024-12-03 15:50:31 -0600

    Added "Coming soon" to the timeline page

--- src/app/timeline/page.tsx
+++ src/app/timeline/page.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import Footer from "../components/Footer";
 export default function Timeline() {
   return (
-    <main className="relative pt-[calc(3.5rem+1px)] md:pt-14 mt-6 md:mt-8 pb-0">
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pb-0">
-        <h1 className="text-4xl text-white mb-6">Timeline</h1>
-        {/* Timeline content here */}
-      </div>
+    <div className="min-h-[calc(100vh-3.5rem)] flex flex-col">
+      <main className="flex-grow relative pt-[calc(3.5rem+1px)] md:pt-14 mt-6 md:mt-8">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <h1 className="text-4xl text-white mb-6">Timeline</h1>
+          <p className="text-white">Coming soon.</p>
+        </div>
+      </main>
       <Footer />
-    </main>
+    </div>
   );
 }
