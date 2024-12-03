25d4ccf5a3515f452a8b0a8158b0a6ee5bed3d32
L.B. Deyo <lbdeyo@gmail.com>
2024-12-03 15:54:37 -0600

    Increasing the size of the Coming Soon text

--- src/app/timeline/page.tsx
+++ src/app/timeline/page.tsx
@@ -6,7 +6,7 @@ export default function Timeline() {
       <main className="flex-grow relative pt-[calc(3.5rem+1px)] md:pt-14 mt-6 md:mt-8">
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <h1 className="text-4xl text-white mb-6">Timeline</h1>
-          <p className="text-white">Coming soon.</p>
+          <p className="text-white text-2xl">Coming soon.</p>
         </div>
       </main>
       <Footer />
