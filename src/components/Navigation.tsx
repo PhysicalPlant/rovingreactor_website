59b7849e7664353a03441361dc754d390a9a7d28
lbdeyo <lbdeyo@gmail.com>
2025-03-20 16:30:28 -0500

    Committing the nav

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,6 +1,8 @@
-const SHOW_SOUP_SECTION = process.env.NEXT_PUBLIC_SHOW_SOUP_SECTION === 'true';
+const SHOW_SOUP_SECTION = process.env.NEXT_PUBLIC_SHOW_SOUP_SECTION === 'true' || false;
 
 const menuItems = [
+    // Temporarily commented out until env var is set up
+    /*
     ...(SHOW_SOUP_SECTION ? [{
         title: "Soup",
         path: "/soup",
@@ -9,4 +11,5 @@ const menuItems = [
             // ... other soup menu items
         ]
     }] : [])
+    */
 ]; 
