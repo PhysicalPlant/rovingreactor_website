391c2375e46288472d29c5e745aef398efcc30a8
lbdeyo <lbdeyo@gmail.com>
2026-01-08 14:00:04 -0600

    Adding Sanity Studio

---
+++ src/sanity/schemaTypes/update.ts
@@ -0,0 +1,27 @@
+import { defineType, defineField } from 'sanity'
+
+export const update = defineType({
+  name: 'update',
+  title: 'Project Update',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: Rule => Rule.required(),
+    }),
+    defineField({
+      name: 'date',
+      title: 'Date',
+      type: 'date',
+      validation: Rule => Rule.required(),
+    }),
+    defineField({
+      name: 'content',
+      title: 'Content',
+      type: 'array',
+      of: [{ type: 'block' }],
+    }),
+  ],
+})
